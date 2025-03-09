ec949ee2af4ae16bca8e9033888b617df83e6e8d
Magdalena Starczyk <magdastarczyk84@gmail.com>
2025-03-09 14:57:41 +0100

    update

--- modele/model_v1.py
+++ modele/model_v1.py
@@ -5,14 +5,12 @@ from tensorflow.keras import Sequential
 from input_data import class_names, train_ds, val_ds
 from augmentation import data_augmentation
 
-import keras_tuner
-import keras
-
 
 def get_model():
     model = keras.Sequential([
+        keras.layers.Input(shape=(128,128,3)),
         data_augmentation,
-        keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+        keras.layers.Conv2D(64, (3, 3), activation='relu'),
         keras.layers.MaxPooling2D(2, 2),
         keras.layers.Flatten(),
         keras.layers.Dense(len(class_names), activation='softmax')
@@ -27,6 +25,7 @@ model.compile(
     metrics = ['accuracy']
 )
 
+model.summary()
 tensorboard_callback = keras.callbacks.TensorBoard(
     log_dir="callback", histogram_freq=1,
 )
@@ -36,14 +35,13 @@ history = model.fit(
     train_ds,
     validation_data=val_ds,
     epochs=epochs,
-    verbose=2,
-    callbacks=[tensorboard_callback]
+    verbose=2
 )
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
-
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-model.save('model_v1.h5')
+#model.save('h5_files/model_v1.h5')
+
