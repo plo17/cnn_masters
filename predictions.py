758a1ea626eec6ce69af2ab92ef877d7dd5765ee
Magdalena Starczyk <magdastarczyk84@gmail.com>
2025-03-10 20:36:58 +0100

    % does not work

--- predictions.py
+++ predictions.py
@@ -16,6 +16,7 @@ def load_and_preprocess_image(img_path, target_size=(128, 128)):
     """
     img = image.load_img(img_path, target_size=target_size)
     img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
     return img_array
@@ -33,15 +34,17 @@ for img_name in os.listdir(new_images_dir):
         img_array = load_and_preprocess_image(img_path)
 
         predictions = model.predict(img_array)[0]
-        top_3_indices = np.argsort(predictions)[-3:][::-1]
+        print("Min:", np.min(img_array), "Max:", np.max(img_array))
 
+        top_3_indices = np.argsort(predictions)[-3:][::-1]  # Sortowanie i pobranie top 3
+        top_3_probs = predictions[top_3_indices]  # Pobranie prawdopodobieństw
 
         print(f"Zdjęcie: {img_name}")
         for i, idx in enumerate(top_3_indices):
-            class_name = class_names[idx]
-            probability = round(predictions[idx] * 100, 2)
-            print(f"{i + 1}. {class_name}: {probability}%")
+            print(f"{i + 1}. {class_names[idx]}: {round(top_3_probs[i] * 100, 2)}%")
         print("-" * 50)
+
+
 
     except Exception as e:
         print(f"Błąd podczas przetwarzania zdjęcia {img_name}: {e}")
